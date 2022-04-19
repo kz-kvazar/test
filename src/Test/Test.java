adb6014a27a690e25a49dd8c1049b726e2062cf2
kvazar <milniy87@gmail.com>
2022-04-19 12:05:04 +0300

    Первый тест

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,85 +1,22 @@
 package Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Test extends CountLetter {
-
-    public static final String bug = "bug";
+public class Test {
 
     public static void main(String[] args) {
-        //Collections.r
-        ArrayList<String> words = new ArrayList<>();
-        words.add("Hello world!");
-        words.add("Amigo");
-        words.add("Elly");
-        words.add("Kerry");
-        words.add("Bug");
-        words.add("bug");
-        words.add("Easy ug");
-        words.add("Risha");
-
-
-        ArrayList<String> copyWordsFirst = new ArrayList<>(words);
-        ArrayList<String> copyWordsSecond = new ArrayList<>(words);
-        ArrayList<String> copyWordsThird = new ArrayList<>(words);
-
-        removeBugWithFor(copyWordsFirst);
-        removeBugWithWhile(copyWordsSecond);
-        removeBugWithCopy(copyWordsThird);
-
-        copyWordsFirst.forEach(System.out::println);
-        String line = "_________________________";
-        System.out.println(line);
-        copyWordsSecond.forEach(System.out::println);
-        System.out.println(line);
-        copyWordsThird.forEach(System.out::println);
-        System.out.println(line);
+        System.out.println(getShapeNameByCountOfCorners(3));
+        System.out.println(getShapeNameByCountOfCorners(5));
+        System.out.println(getShapeNameByCountOfCorners(1));
     }
 
-    public static void removeBugWithFor(ArrayList<String> list) {
-        //напишите тут ваш код
-        for (int i = 0; list.size() > i; i++) {
-            String str = list.get(i);
-            if (str.equalsIgnoreCase(bug)) {
-                list.remove(str);
-                i--;
-            }
-
-        }
-    }
-
-    public static void removeBugWithWhile(ArrayList<String> list) {
-        //напишите тут ваш код
-        list.removeIf(bug::equalsIgnoreCase);
-    }
-
-    public static void removeBugWithCopy(ArrayList<String> list) {
-        //напишите тут ваш код
-        ArrayList<String> newList = new ArrayList<>(list);
-        for (String S : newList) {
-            if (bug.equalsIgnoreCase(S)) {
-                list.remove(S);
-
-            }
-        }
-
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("Серега", 21);
-        map.put("Николай", 22);
-        map.put("Иван Петрович", 48);
-        map.put("Анюта", null);
-
-        for (String key: map.keySet())
-        {
-            Integer value = map.get(key);
-            System.out.println(key + " --> " + value);
-        }
-
+    public static String getShapeNameByCountOfCorners(int countOfCorner) {
+        return switch (countOfCorner) {
+            case 3 -> "Треугольник";
+            case 4 -> "Четырехугольник";
+            case 5 -> "Пятиугольник";
+            case 6 -> "Шестиугольник";
+            case 7 -> "Семиугольник";
+            case 8 -> "Восьмиугольник";
+            default -> "Другая фигура";
+        };
     }
 }
-
-
-
-
-
